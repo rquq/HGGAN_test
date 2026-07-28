9c2a331cf6cfbf707e3e82ad561f67cb1b999a49
rquq <williamwrost617@gmail.com>
2026-07-28 11:04:34 +0700

    Update model configuration and training runs

--- train.py
+++ train.py
@@ -17,8 +17,8 @@ def seed_everything(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = False
+    torch.backends.cudnn.benchmark = True
 
 
 if __name__ == "__main__":
